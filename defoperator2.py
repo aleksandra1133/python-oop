b9573dcaffea4250eaf4dbce2100f1cdddcf1a72
aleksandra1133 <159744607+aleksandra1133@users.noreply.github.com>
2024-06-07 23:49:18 +0500

    Add files via upload

---
+++ defoperator2.py
@@ -0,0 +1,14 @@
+class ReversedList:
+    def __init__(self, lst):
+        self.lst = lst
+
+    def __len__(self):
+        return len(self.lst)
+
+    def __getitem__(self, index):
+        return self.lst[len(self.lst) - 1 - index]
+
+
+rl = ReversedList([10])
+print(len(rl))
+print(rl[0])
